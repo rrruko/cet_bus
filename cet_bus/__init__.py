96002443c6aad9d7b1a7e1a93d52bd1a67e96e2b
Ethan Jones <joneetha@oregonstate.edu>
2020-01-17 09:10:17 -0800

    init

---
+++ cet_bus/__init__.py
@@ -0,0 +1,2 @@
+from .bus_routes import enumerate_shapes, guess_route, route_histo, passes
+from .geo import Point, Segment, Polyline
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cet_bus',
+      version='0.1',
+      description='Functions for processing CET bus data',
+      url='',
+      author='Ethan Jones',
+      author_email='rukokarasu@gmail.com',
+      license='MIT',
+      packages=['cet_bus'],
+      zip_safe=False)
